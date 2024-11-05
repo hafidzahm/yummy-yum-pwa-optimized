c94324cccf822e5fd9bafaeef259ffd4a541ff26
Abdul <hafizhahm123@gmail.com>
2024-11-05 16:56:55 +0700

    add array test implement from contract

---
+++ tests/favoriteRestoArray.test.js
@@ -0,0 +1,47 @@
+import { itActsAsFavoriteRestaurantModel } from './helpers/contracts/favoriteRestoContract';
+
+let favoriteRestaurants = [];
+
+const FavoriteRestaurantArray = {
+  getRestaurant(id) {
+    if (!id) {
+      return;
+    }
+
+    return favoriteRestaurants.find((restaurant) => restaurant.id == id);
+  },
+
+  getAllRestaurants() {
+    return favoriteRestaurants;
+  },
+
+  putRestaurant(restaurant) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!restaurant.hasOwnProperty('id')) {
+      return;
+    }
+
+    // pastikan id ini belum ada dalam daftar favoriteRestaurants
+    if (this.getRestaurant(restaurant.id)) {
+      return;
+    }
+
+    favoriteRestaurants.push(restaurant);
+  },
+
+  deleteRestaurant(id) {
+    // cara boros menghapus resto dengan meng-copy resto yang ada
+    // kecuali resto dengan id == id
+    favoriteRestaurants = favoriteRestaurants.filter(
+      (restaurant) => restaurant.id != id
+    );
+  },
+};
+
+describe('Favorite Restaurant Array Contract Test Implementation', () => {
+  afterEach(() => {
+    favoriteRestaurants = [];
+  });
+
+  itActsAsFavoriteRestaurantModel(FavoriteRestaurantArray);
+});
