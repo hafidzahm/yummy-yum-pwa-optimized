ea38e2d4dca67cddff863dc086049f8a305cae74
Abdul <hafizhahm123@gmail.com>
2024-11-08 16:15:33 +0700

    delete unnecessary tests

--- tests/restaurant-search/restaurantSearchedShow.test.js
+++ tests/restaurant-search/restaurantSearchedShow.test.js
@@ -13,21 +13,6 @@ describe('showing all searached restaurants', () => {
   });
 
   describe('when no restaurant', () => {
-    it('should render the information that no restaurant', () => {
-      const allSearchedRestaurants = {
-        getAllRestaurants: jest.fn().mockImplementation(() => []),
-      };
-      const presenter = new RestaurantSearchedShowPresenter({
-        view,
-        allSearchedRestaurants,
-      });
-
-      const restaurants = [];
-      presenter._displayRestaurants(restaurants);
-      expect(
-        document.querySelectorAll('.restaurant-item__not__found').length
-      ).toEqual(1);
-    });
     it('should ask for all searched restaurant', () => {
       const allSearchedRestaurants = {
         getAllRestaurants: jest.fn().mockImplementation(() => []),
@@ -62,31 +47,6 @@ describe('showing all searached restaurants', () => {
   });
 
   describe('when restaurants exist', () => {
-    it('should render the restaurants', () => {
-      const allSearchedRestaurants = {
-        getAllRestaurants: jest.fn().mockImplementation(() => []),
-      };
-      const presenter = new RestaurantSearchedShowPresenter({
-        view,
-        allSearchedRestaurants,
-      });
-
-      presenter._displayRestaurants([
-        {
-          id: 111,
-          name: 'resto abc',
-          city: 'ABC',
-          rating: 3.4,
-        },
-        {
-          id: 222,
-          name: 'ada juga resto abcde',
-          city: 'bwandung',
-          rating: 4.5,
-        },
-      ]);
-      expect(document.querySelectorAll('.restaurant').length).toEqual(2);
-    });
     it('should show the restaurants', (done) => {
       document
         .getElementById('restaurants')
