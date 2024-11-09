a03ee377faa15f28e11f7b7173b51255079a63c3
Abdul <hafizhahm123@gmail.com>
2024-11-09 13:55:02 +0700

    e2e: adding restaurants to favorite

--- e2e/Adding_Restaurants_to_Favorite.test.js
+++ e2e/Adding_Restaurants_to_Favorite.test.js
@@ -8,3 +8,19 @@ Scenario('showing empty favorited restaurants', ({ I }) => {
   I.seeElement('.empty-favorite');
   I.see('Anda tidak punya Restoran Favorit.', '.text-info');
 });
+
+Scenario('adding restaurants to favorite', ({ I }) => {
+  I.see('Anda tidak punya Restoran Favorit.', '.text-info');
+
+  I.amOnPage('/');
+
+  I.seeElement('#card-item__name a');
+  I.click(locate('#card-item__name a').first());
+
+  I.seeElement('.favoriteButton');
+  I.click('.favoriteButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+
+});
